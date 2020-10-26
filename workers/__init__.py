a6a2c5c28d86e32fee4d9cca404226a587872e38
Phạm Xuân Đức <ducpx@vccloud.vn>
2020-10-26 15:15:55 +0700

    fork thread

--- workers/__init__.py
+++ workers/__init__.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 import click
@@ -17,9 +18,27 @@ def get_users():
     postgres_db = Postgres(uri=BaseConfig.POSTGRES_URI)
 
     while True:
+        print('Length thread: %s' % len(threading.enumerate()))
+        for t in threading.enumerate():
+            print('for thread name: %s' % t.getName())
         session = postgres_db.start_session()
-        users = session.query(postgres_models.User).all()
-        session.close()
+        thread = MessageThread(session)
+        thread.start()
+        time.sleep(3)
+
+
+class MessageThread(threading.Thread):
+
+    def __init__(self, session):
+        threading.Thread.__init__(self)
+        self.session = session
+
+    def run(self):
+        print('Thread run name: %s' % self.getName())
+        time.sleep(5)
+        users = self.session.query(postgres_models.User).all()
+        self.session.close()
         for u in users:
             print('User %s, name %s' % (u.id, u.name))
         time.sleep(5)
+        print('MessageThread %s exit' % self.getName())
